cb571543a9db2d3703fba49e77e480fcf1c3a677
Alexandre Ghiti <alexghiti@rivosinc.com>
2024-08-01 21:14:04 +0200

    riscv: Re-introduce global icache flush in patch_text_XXX()
    commit edf2d546bfd6 ("riscv: patch: Flush the icache right after
    patching to avoid illegal insns") mistakenly removed the global icache
    flush in patch_text_nosync() and patch_text_set_nosync() functions, so
    reintroduce them.
    
    Fixes: edf2d546bfd6 ("riscv: patch: Flush the icache right after patching to avoid illegal insns")
    Reported-by: Samuel Holland <samuel.holland@sifive.com>
    Closes: https://lore.kernel.org/linux-riscv/a28ddc26-d77a-470a-a33f-88144f717e86@sifive.com/
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>
    Reviewed-by: Samuel Holland <samuel.holland@sifive.com>
    Reviewed-by: Charlie Jenkins <charlie@rivosinc.com>
    Link: https://lore.kernel.org/r/20240801191404.55181-1-alexghiti@rivosinc.com
    Signed-off-by: Palmer Dabbelt <palmer@rivosinc.com>

--- arch/riscv/kernel/patch.c
+++ arch/riscv/kernel/patch.c
@@ -200,10 +200,11 @@ NOKPROBE_SYMBOL(patch_insn_set);
 
 int patch_text_set_nosync(void *addr, u8 c, size_t len)
 {
-	u32 *tp = addr;
 	int ret;
 
-	ret = patch_insn_set(tp, c, len);
+	ret = patch_insn_set(addr, c, len);
+	if (!ret)
+		flush_icache_range((uintptr_t)addr, (uintptr_t)addr + len);
 
 	return ret;
 }
@@ -232,10 +233,11 @@ NOKPROBE_SYMBOL(patch_insn_write);
 
 int patch_text_nosync(void *addr, const void *insns, size_t len)
 {
-	u32 *tp = addr;
 	int ret;
 
-	ret = patch_insn_write(tp, insns, len);
+	ret = patch_insn_write(addr, insns, len);
+	if (!ret)
+		flush_icache_range((uintptr_t)addr, (uintptr_t)addr + len);
 
 	return ret;
 }
